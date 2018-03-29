baefadc5321672000c48ee1226fa98228fce7237
Jose Reyes <jaeyow@gmail.com>
2018-03-30 08:38:06 +1100

    File Document picker in iOS

--- js/components/login/index.js
+++ js/components/login/index.js
@@ -14,6 +14,7 @@ import {
 import { Field, reduxForm } from "redux-form";
 import { setUser } from "../../actions/user";
 import styles from "./styles";
+import { DocumentPicker, DocumentPickerUtil } from 'react-native-document-picker';
 
 const background = require("../../../images/shadow.png");
 
@@ -79,9 +80,9 @@ class Login extends Component {
         />
         {hasError
           ? <Item style={{ borderColor: "transparent" }}>
-              <Icon active style={{ color: "red", marginTop: 5 }} name="bug" />
-              <Text style={{ fontSize: 15, color: "red" }}>{error}</Text>
-            </Item>
+            <Icon active style={{ color: "red", marginTop: 5 }} name="bug" />
+            <Text style={{ fontSize: 15, color: "red" }}>{error}</Text>
+          </Item>
           : <Text />}
       </Item>
     );
@@ -97,9 +98,15 @@ class Login extends Component {
                 <Field name="password" component={this.renderInput} />
                 <Button
                   style={styles.btn}
+                  onPress={() => this.showFilePicker()}
+                >
+                  <Text>Select Files</Text>
+                </Button>
+                <Button
+                  style={styles.btn}
                   onPress={() => this.props.navigation.navigate("Home")}
                 >
-                  <Text>Login</Text>
+                  <Text>Login Now</Text>
                 </Button>
               </View>
             </Image>
@@ -107,6 +114,52 @@ class Login extends Component {
         </View>
       </Container>
     );
+  }
+
+  showFilePicker = () => {
+    // iPhone/Android
+    DocumentPicker.show({
+      filetype: [DocumentPickerUtil.images()],
+    }, (error, res) => {
+      // Android
+      console.log(
+        res.uri,
+        res.type, // mime type
+        res.fileName,
+        res.fileSize
+      );
+      var media = {
+        uri: res.uri,
+        type: res.type,
+        name: res.fileName
+      };
+      
+      var data = new FormData();
+      data.append("file", media);
+      const fileArray = [];
+      
+      fetch('https://mobile_push_notifier.cfapps.io/service-request/attachments', {
+          method: 'POST',
+          body: data,
+      })
+          .then(
+              (response) => {
+                  response.json()
+                      .then(
+                          (success) => {
+                              // tslint:disable-next-line:no-console
+                              console.log(success[0].fileKey);
+                              alert(success[0].fileKey);
+                          })
+                      .catch(
+                          (error) => {
+                              // tslint:disable-next-line:no-console
+                              console.log(error);
+                              alert('failed');
+                          }
+                      );
+              });
+    });
   }
 }
 const LoginSwag = reduxForm(
